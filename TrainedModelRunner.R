35cfcd794565b6475740bd0c226e6a3d5ccf6b81
rjk <rjk>
2018-05-17 15:22:50 +1000

    Simple runner for a trained model.

---
+++ TrainedModelRunner.R
@@ -0,0 +1,44 @@
+library(gym)
+
+source("DQNAgent.R")
+
+# Open AI Gym Environment.
+remoteURL <- "http://127.0.0.1:5000"
+client <- create_GymClient(remoteURL)
+
+# Cart Pole
+environmentID <- "CartPole-v0"
+instanceID <- env_create(client, environmentID)
+
+actionSpace <- env_action_space_info(client, instanceID)
+stateSpace <- env_observation_space_info(client, instanceID)$shape[[1]]
+
+state <- unlist(env_reset(client, instanceID))
+score <- 0
+
+while (TRUE) {
+  
+  score <- score + 1
+  
+  thisAction <- act(state = state,
+                    model = model,
+                    epsilon = 0,
+                    availableActions = actionSpace)
+  
+  state <- env_step(
+    client,
+    instanceID,
+    thisAction,
+    render = TRUE
+  )
+  
+  if (state$done) {
+    print(paste("Score:", score))
+    break;
+  }
+  
+  Sys.sleep(0.25)
+  state <- unlist(nextState$observation)
+}
+
+env_monitor_close(client, instanceID)
